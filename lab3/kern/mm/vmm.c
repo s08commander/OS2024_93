a267f2393d59a9aeec4c58d38be3b1f0af41bbfe
s08commander <133472880+s08commander@users.noreply.github.com>
2024-11-17 12:03:41 +0800

    练习三

--- lab3/kern/mm/vmm.c
+++ lab3/kern/mm/vmm.c
@@ -383,7 +383,7 @@ do_pgfault(struct mm_struct *mm, uint_t error_code, uintptr_t addr) {
             goto failed;
         }
     } else {
-        /*LAB3 EXERCISE 3: YOUR CODE
+        /*LAB3 EXERCISE 3: 2211765
         * 请你根据以下信息提示，补充函数
         * 现在我们认为pte是一个交换条目，那我们应该从磁盘加载数据并放到带有phy addr的页面，
         * 并将phy addr与逻辑addr映射，触发交换管理器记录该页面的访问情况
@@ -407,6 +407,21 @@ do_pgfault(struct mm_struct *mm, uint_t error_code, uintptr_t addr) {
             //logical addr
             //(3) make the page swappable.
             page->pra_vaddr = addr;
+		if(swap_in(mm, addr, &page) == 0){
+
+            if(page_insert(mm->pgdir,page,addr,perm)==0){
+                swap_map_swappable(mm,addr,page,1);
+                page->pra_vaddr = addr;
+            }
+	    else{
+                cprintf("page_insert failed for addr 0x%x\n", addr);
+                goto failed;
+            }
+            }
+            else{
+            cprintf("swap_in failed for addr 0x%x\n", addr);
+            goto failed;
+            }
         } else {
             cprintf("no swap_init_ok but ptep is %x, failed\n", *ptep);
             goto failed;
